0d3e24abba249ac95c25d01f24f79ecb2e29e711
nachogl1 <ignacio.gonzalez@codurance.com>
2022-06-28 17:02:28 +0100

    first commit

---
+++ src/main/java/com/codurance/testacademyapp/TestAcademyAppApplication.java
@@ -0,0 +1,13 @@
+package com.codurance.testacademyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAcademyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAcademyAppApplication.class, args);
+    }
+
+}
